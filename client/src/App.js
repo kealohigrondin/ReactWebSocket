9fe1ae1bcac72e441f93fa4eaaff360110695d2c
Kealohi Grondin <jacob.grondin@wsu.edu>
2023-06-02 09:47:37 -0700

    still receiving two messages for one send

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,48 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
-
 const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [message, setMessage] = useState("");
+  const [inMessages, setInMessages] = useState([]);
+  const [roomNumber, setRoomNumber] = useState(0);
 
   const sendMessage = () => {
-    console.log("sending message to server");
     if (message !== "") socket.emit("send_message", { message });
   };
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      console.log(data.message);
+        setInMessages(inMessages => [...inMessages, data.message]);
     });
-  }, []);
+  }, [socket]);
 
   return (
     <div className="App">
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "0.5rem",
-          alignItems: "center",
-        }}
-      >
+      <div className="flex flex-col items-center gap">
+        <p className="text-white text-2xl">Room {roomNumber}</p>
+        <input
+          type="number"
+          placeholder="enter room number"
+          onChange={(event) => setRoomNumber(event.target.value)}
+        />
         <input
           placeholder="message..."
-          style={{ width: "25%" }}
+          className="w-1/4 mt-8 mb-2 p-2 rounded-sm"
           onChange={(event) => setMessage(event.target.value)}
         />
-        <button onClick={sendMessage}>Send Message</button>
+        <button
+          onClick={sendMessage}
+          className="bg-blue-700 text-white py-2 px-6 mb-10 rounded-lg"
+        >
+          Send Message
+        </button>
+        {inMessages}
+        {inMessages.map((inMessage, index) => (
+          <p key={index} className="text-lg text-white my-2">
+            {inMessage}
+          </p>
+        ))}
       </div>
     </div>
   );
